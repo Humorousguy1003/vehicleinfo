cef9410248be8a03bf4fb8c85c15af5a0a0ce870
Humorousguy1003 <jackchao1003@gmail.com>
2023-01-12 13:55:59 +0100

    logo updated

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 export const Footer = () => {
-    return(
+    return (
         <div className='row'>
             <div className="footercss flex justify-center flex-col">
                 <div className='w-24 h-24 pt-5 '>
-                    <img src="./logo.png" className="image"/>
+                    <img src="./logo-real.png" className="image" />
                 </div>
-                    
+
                 <div className="space-x-5 text-black  pt-5">
                     <a className="chakra-link css-spn4bz  text-black" href="/">Home</a>
                     <a className="chakra-link css-spn4bz  text-black" href="/search">Auctions history</a>
--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -14,7 +14,7 @@ function Header() {
     <div className="w-full border-b border-solid border	border-[#CBD5E0] pt-3 pb-3 bg-white block ">
       <div className="container flex justify-between items-center">
         <div className="header-left-part flex items-center space-x-7 float-left ">
-          <img className="w-auto h-16 cursor-pointer" src="./logo.png" onClick={handleSearch} />
+          <img className="w-auto h-16 cursor-pointer" src="./logo-real.png" onClick={handleSearch} />
           <select className=" langsel w-24 rounded text-base font-medium ml-2.5 outline-0	">
             <option value={1}>English</option>
             <option value={1}>Russion</option>
